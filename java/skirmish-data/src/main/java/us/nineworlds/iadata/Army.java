16d59eb4d40fd9477b7f5fb55f3df4f456e1b189
David Carver <kingargyle@users.noreply.github.com>
2017-10-23 10:21:42 -0400

    Add Descriptions to fields.

--- java/skirmish-data/src/main/java/us/nineworlds/iadata/Army.java
+++ java/skirmish-data/src/main/java/us/nineworlds/iadata/Army.java
@@ -3,15 +3,18 @@ package us.nineworlds.iadata;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import us.nineworlds.iadata.deployment.Deployments;
 
 public class Army {
 	
 	@JsonProperty(required=true, value="name")
+	@JsonPropertyDescription("A required field that is a human readable name for the army.")
 	private String name;
 
 	@JsonProperty(required=true, value="deployments")
+	@JsonPropertyDescription("A required list of one or more deployment groups that describe the army.")
 	private List<Deployments> deployments;
 
 	public String getName() {
