9dba41897a0f27c5995e6ee74665d778a819c8c7
David Carver <kingargyle@users.noreply.github.com>
2017-10-23 10:21:03 -0400

    Add Descriptions to fields

--- java/skirmish-data/src/main/java/us/nineworlds/iadata/IASpec.java
+++ java/skirmish-data/src/main/java/us/nineworlds/iadata/IASpec.java
@@ -3,18 +3,22 @@ package us.nineworlds.iadata;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import us.nineworlds.iadata.command.CommandCards;
 
 public class IASpec {
 
 	@JsonProperty(required=true, value="iaspecversion")
+	@JsonPropertyDescription("The versions number of the specification implemented. Currently this is 0.5.0.  This is a required field.")
 	private String version;
 	
 	@JsonProperty(required=true, value="army")
+	@JsonPropertyDescription("An army contains a deployments group, which contains a list of deployment cards. This is a required field")
 	private Army army;
 	
 	@JsonProperty(required=true, value="commandCards")
+	@JsonPropertyDescription("A list of command cards for the army described in this specification. This is a required field.")
 	List<CommandCards> commandCards;
 
 	public String getVersion() {
@@ -40,6 +44,4 @@ public class IASpec {
 	public void setCards(List<CommandCards> commandCards) {
 		this.commandCards = commandCards;
 	}
-	
-	
 }
