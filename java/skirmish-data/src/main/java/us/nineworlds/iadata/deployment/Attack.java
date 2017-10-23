3c99c5f18ededfa98492a83483b1ec359ee84032
David Carver <kingargyle@users.noreply.github.com>
2017-10-23 10:24:36 -0400

    Add Description to fields

--- java/skirmish-data/src/main/java/us/nineworlds/iadata/deployment/Attack.java
+++ java/skirmish-data/src/main/java/us/nineworlds/iadata/deployment/Attack.java
@@ -3,16 +3,19 @@ package us.nineworlds.iadata.deployment;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 import us.nineworlds.iadata.enums.AttackDice;
 import us.nineworlds.iadata.enums.AttackType;
 
 public class Attack {
 	
-	@JsonProperty("type")
+	@JsonProperty(required=true, value="type")
+	@JsonPropertyDescription("A required enumerated value that describes the attack type.  melee or range")
 	private AttackType attackType;
 	
-	@JsonProperty("dice")
+	@JsonProperty(required=true, value="dice")
+	@JsonPropertyDescription("A list of attack dice used to make up the attack pool. ")
 	private List<AttackDice> dicePool;
 
 	public AttackType getAttackType() {
