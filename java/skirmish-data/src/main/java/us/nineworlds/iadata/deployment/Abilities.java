b1b57378974e137cab6dae3e64c3951acbf2bda7
David Carver <kingargyle@users.noreply.github.com>
2017-10-23 10:22:28 -0400

    Add Descriptions to fields

--- java/skirmish-data/src/main/java/us/nineworlds/iadata/deployment/Abilities.java
+++ java/skirmish-data/src/main/java/us/nineworlds/iadata/deployment/Abilities.java
@@ -1,10 +1,12 @@
 package us.nineworlds.iadata.deployment;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyDescription;
 
 public class Abilities {
 
 	@JsonProperty("ability")
+	@JsonPropertyDescription("A field that describes the abilities for a deployment card.")
 	private AbilityGroup ability;
 
 	public AbilityGroup getAbility() {
